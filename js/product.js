533337efa8e7f75bb48bf830f49c86f5c52d3b37
Jose <sesejose@gmail.com>
2022-05-02 12:26:23 +0200

    colors-bags-shop-product-page-js

--- js/product.js
+++ js/product.js
@@ -17,7 +17,8 @@ fetch(url)
   });
 
 function handleBag(data) {
-  //Bag is an array, so here I am looping through the array that comes back from the API. Extracting all the objects from the array - one by one !
+  //Lasse says that is not neccesary when is a single object - by ID
+  //Lucas / Bag is an array, so here I am looping through the array that comes back from the API. Extracting all the objects from the array - one by one !
   // data.forEach(showBag);
 }
 
@@ -48,4 +49,19 @@ function showBag(bag) {
   document.querySelector(
     "ul.thumbs li:last-child"
   ).style.backgroundImage = `url(${bag.thumb4.guid})`;
+
+  const colors = bag._embedded["wp:term"][1]; //Define the variable colors
+  if (colors.length) {
+    //If colors is an array go through this
+    document.querySelector(".colors-exchange-container").textContent = ""; //In document select color-exchange-container class and add textContent
+    const ulNew = document.createElement("ul"); //Define a variable ulNew and creates an element in the DOM - appended at the end
+    colors.forEach((color) => {
+      // Then for colors (argument of if) apllies forEach (because color is an Array) and gives a function (without name neither argument)
+      const liNew = document.createElement("li"); //That function defines the variable liNew
+      liNew.style.backgroundColor = color.name; // Then for that variable liNew style with background color
+      ulNew.appendChild(liNew); //And here inserts the liNew variable in ulNew
+    });
+    document.querySelector(".colors-exchange-container").appendChild(ulNew); //Then select the template and then the class where we want the ulNew be appends
+    // that's all, easy?
+  }
 }
--- js/shop.js
+++ js/shop.js
@@ -36,6 +36,21 @@ function showBag(bag) {
     .querySelector(".price-add-to-card a")
     .setAttribute("href", `product.html?id=${bag.id}`);
 
+  const colors = bag._embedded["wp:term"][1]; //Define the variable colors
+  if (colors.length) {
+    //If colors is an array go through this
+    copy.querySelector(".colors-exchange-container").textContent = ""; //In the template copy (document) select color value and add textContent
+    const ulNew = document.createElement("ul"); //Define a variable ulNew and creates an element in the DOM - appended at the end
+    colors.forEach((color) => {
+      // Then for colors (argument of if) apllies forEach (because color is an Array) and gives a function (without name neither argument)
+      const liNew = document.createElement("li"); //That function defines the variable liNew
+      liNew.style.backgroundColor = color.name; // Then for that variable liNew style with background color
+      ulNew.appendChild(liNew); //And here inserts the liNew variable in ulNew
+    });
+    copy.querySelector(".colors-exchange-container").appendChild(ulNew); //Then select the template and then the class where we want the ulNew be appends
+    // that's all, easy?
+  }
+
   const parent = document.querySelector("#shop-grid");
   parent.appendChild(copy);
 }
