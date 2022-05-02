7b84a19c3b4d177da642742c101d3051c30d1323
Jose <sesejose@gmail.com>
2022-05-02 11:14:59 +0200

    Lasse-2-about-shop-backgroundImage

--- js/product.js
+++ js/product.js
@@ -1,7 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-const url = "http://sesejose.com/kea/wp_silfen/wp-json/wp/v2/bag?_embed" + id;
+const url =
+  "http://sesejose.com/kea/wp_silfen/wp-json/wp/v2/bag/" + id + "?_embed";
 
 console.log("url");
 
@@ -12,12 +13,12 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
-    handleBag(data);
+    showBag(data);
   });
 
 function handleBag(data) {
   //Bag is an array, so here I am looping through the array that comes back from the API. Extracting all the objects from the array - one by one !
-  data.forEach(showBag);
+  // data.forEach(showBag);
 }
 
 function showBag(bag) {
@@ -31,12 +32,20 @@ function showBag(bag) {
     bag.excerpt.rendered;
   document.querySelector(".material").textContent = bag.material;
   document.querySelector(".dimensions").textContent = bag.dimensions;
-  document.querySelector("ul.thumbs li:first-child").style.backgroundImage =
-    "url('bag.thumb3.guid')";
-  document.querySelector("ul.thumbs li:nth-child(2)").style.backgroundImage =
-    "url('bag.thumb3.guid')";
-  document.querySelector("ul.thumbs li:nth-child(3)").style.backgroundImage =
-    "url('bag.thumb3.guid')";
-  document.querySelector("ul.thumbs li:last-child").style.backgroundImage =
-    "url('bag.thumb3.guid')";
+
+  document.querySelector(
+    "ul.thumbs li:first-child"
+  ).style.backgroundImage = `url(${bag.thumb1.guid})`;
+
+  document.querySelector(
+    "ul.thumbs li:nth-child(2)"
+  ).style.backgroundImage = `url(${bag.thumb2.guid})`;
+
+  document.querySelector(
+    "ul.thumbs li:nth-child(3)"
+  ).style.backgroundImage = `url(${bag.thumb3.guid})`;
+
+  document.querySelector(
+    "ul.thumbs li:last-child"
+  ).style.backgroundImage = `url(${bag.thumb4.guid})`;
 }
--- js/shop.js
+++ js/shop.js
@@ -21,11 +21,17 @@ function showBag(bag) {
   const copy = template.cloneNode(true);
   copy.querySelector(".bag-name").textContent = bag.title.rendered;
   copy.querySelector(".bag-price").textContent = bag.price;
-  document.querySelector("a").style.backgroundImage = "url('bag.thumb1.guid')";
+
+  document.querySelector(".test").src = bag.thumb1.guid;
+
+  // document.querySelector(".product-img-shop a").style.backgroundImage = `url(${bag.thumb1.guid})`;
 
   copy
     .querySelector(".price-add-to-card a")
     .setAttribute("href", `product.html?id=${bag.id}`);
+
+  // copy.querySelector(".product-image-shop a").setAttribute("href", `product.html?id=${bag.id}`);
+
   const parent = document.querySelector("#shop-grid");
   parent.appendChild(copy);
 }
