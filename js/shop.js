940c93fcecbd2f7b14fa421505f6b570ad6d1653
Jose <sesejose@gmail.com>
2022-05-02 11:47:10 +0200

    shop-js

--- js/shop.js
+++ js/shop.js
@@ -22,15 +22,19 @@ function showBag(bag) {
   copy.querySelector(".bag-name").textContent = bag.title.rendered;
   copy.querySelector(".bag-price").textContent = bag.price;
 
-  document.querySelector(".test").src = bag.thumb1.guid;
+  // copy.querySelector(".test").src = bag.thumb1.guid;
 
-  // document.querySelector(".product-img-shop a").style.backgroundImage = `url(${bag.thumb1.guid})`;
+  copy.querySelector(
+    ".product-img-shop a"
+  ).style.backgroundImage = `url(${bag.thumb1.guid})`;
+
+  copy
+    .querySelector(".product-img-shop a")
+    .setAttribute("href", `product.html?id=${bag.id}`);
 
   copy
     .querySelector(".price-add-to-card a")
     .setAttribute("href", `product.html?id=${bag.id}`);
-
-  // copy.querySelector(".product-image-shop a").setAttribute("href", `product.html?id=${bag.id}`);
 
   const parent = document.querySelector("#shop-grid");
   parent.appendChild(copy);
